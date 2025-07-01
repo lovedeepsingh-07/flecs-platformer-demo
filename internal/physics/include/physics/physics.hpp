93f5aebb0b47fb71704252bd858acb83177fe924
lovedeepsingh-07 <lovedeepsinghbrar07@gmail.com>
2025-07-01 12:00:25 +0530

    feat: basic physics setup(gravity, jumping)

--- internal/physics/include/physics/physics.hpp
+++ internal/physics/include/physics/physics.hpp
@@ -1,29 +1,28 @@
 #pragma once
 
+#include "raymath.h"
 #include <vector>
+
 namespace Physics {
 
-struct Vector2 {
-    float x;
-    float y;
+
+struct Object {
+    Vector2 position;
+    Vector2 dimensions;
+    Vector2 velocity;
+    Vector2 force;
+    float mass;
 };
 
-class Body {
+class World {
   private:
-    Vector2 m_position;
-    Vector2 m_size;
+    std::vector<Object *> m_objects;
 
   public:
-    Body(Vector2 position, Vector2 size);
-};
-
-class PhysicalWorld {
-  private:
-    std::vector<Body> m_bodies;
-
-  public:
-    auto create_body(Vector2 position, Vector2 size) -> Body &;
-    auto step() -> void;
+    Vector2 world_gravity;
+    auto add_object(Object *object) -> void;
+    auto remove_object(Object *object) -> void;
+    auto step(float delta_time) -> void;
 };
 
 } // namespace Physics
--- internal/physics/src/physics.cpp
+++ internal/physics/src/physics.cpp
@@ -1,11 +1,34 @@
 #include "physics/physics.hpp"
+#include "raymath.h"
+#include <algorithm>
 
 namespace Physics {
-Body::Body(Vector2 position, Vector2 size)
-: m_position(position), m_size(size) {}
+auto World::add_object(Object *object) -> void {
+    if (object != nullptr) {
+        m_objects.push_back(object);
+    }
+};
+auto World::remove_object(Object *object) -> void {
+    if (object != nullptr) {
+        auto iter = std::ranges::find(m_objects, object);
+        if (iter != m_objects.end()) {
+            m_objects.erase(iter);
+        }
+    }
+};
+auto World::step(float delta_time) -> void {
+    for (Object *object : m_objects) {
+        object->force =
+            Vector2Add(object->force, Vector2Scale(world_gravity, object->mass));
 
-auto PhysicalWorld::create_body(Vector2 position, Vector2 size) -> Body & {
-    m_bodies.emplace_back(position, size);
-    return m_bodies.back();
+        Vector2 acceleration = Vector2Scale(object->force, 1.0F / object->mass);
+
+        object->velocity =
+            Vector2Add(object->velocity, Vector2Scale(acceleration, delta_time));
+        object->position =
+            Vector2Add(object->position, Vector2Scale(object->velocity, delta_time));
+
+        object->force = Vector2(0, 0);
+    }
 };
 } // namespace Physics
