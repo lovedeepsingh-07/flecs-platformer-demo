8fc0033aa8aee94fe96d3ef14b49e2a1780b7ca9
lovedeepsingh-07 <lovedeepsinghbrar07@gmail.com>
2025-07-13 15:44:18 +0530

    refactor: some code cleanup

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
         ctx = {
           package = {
             name = "game";
-            version = "0.0.1";
+            version = "0.1.0";
             src = ./.;
           };
           build-deps = [ ];
---
+++ src/main.cc
@@ -0,0 +1,24 @@
+#define CLAY_IMPLEMENTATION
+#include "clay/clay.h"
+#include "clay/renderers/raylib/clay_renderer_raylib.c"
+#include "raylib.h"
+
+int main() {
+    Clay_Raylib_Initialize(0, 0, "game", FLAG_WINDOW_UNDECORATED);
+    int screen_width = GetMonitorWidth(0);
+    int screen_height = GetMonitorHeight(0);
+    SetWindowSize(screen_width, screen_height);
+    SetTargetFPS(75);
+
+    while (!WindowShouldClose()) {
+        float delta_time = GetFrameTime();
+
+
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        EndDrawing();
+    }
+
+    Clay_Raylib_Close();
+}
